c69ac7f9781bca479f8eb48297da8a1004357fa5
SMMA2002 <114697680+SMMA2002@users.noreply.github.com>
2023-02-10 20:06:16 -0600

    commit 1

---
+++ python/ArrayClient.py
@@ -0,0 +1,39 @@
+import Array
+maxSize = 10 # Max size of the array
+arr = Array.Array(maxSize) # Create an array object
+
+arr.insert(90) # Insert 10 items
+arr.insert(98)
+arr.insert(4)
+arr.insert(9)
+arr.insert(55)
+arr.insert(55)
+arr.insert(12.34)
+arr.insert(0)
+arr.insert(1)
+arr.insert(-17)
+
+print("Array containing", len(arr), "items")
+arr.traverse()
+
+print("Search for 12 returns", arr.search(12))
+
+print("Search for 12.34 returns", arr.search(12.34))
+
+print("Deleting 0 returns", arr.delete(0))
+print("Deleting 17 returns", arr.delete(17))
+
+print("Setting item at index 3 to 33")
+arr.set(3, 33)
+
+print("Array after deletions has", len(arr), "items")
+arr.traverse()
+
+print("Get max num", arr.getMaxNum())
+arr.traverse()
+
+print("Delete max num", arr.deleteMaxNum())
+arr.traverse()
+
+print("Delete duplicated num", arr.removeDupes())
+arr.traverse()
